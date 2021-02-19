64ba106cecf1be5ac5077531da7d8e97af8e7312
William Corner <william.corner@gmail.com>
2021-02-19 14:48:18 +0000

    Create TextandImage.js

---
+++ src/modules/TextandImage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { renderHTML } from '../agility/utils'
+
+const RichTextArea = ({ item }) => {
+
+    return (
+        <p>Test</p>
+    );
+}
+
+export default TextandImage;
