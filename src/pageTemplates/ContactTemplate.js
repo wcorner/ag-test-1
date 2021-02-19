4bda1fbc10c403ece63e3ff3884d75ce8c74aff4
William Corner <william.corner@gmail.com>
2021-02-19 14:57:13 +0000

    Update ContactTemplate.js

--- src/pageTemplates/ContactTemplate.js
+++ src/pageTemplates/ContactTemplate.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import ContentZone from '../agility/components/ContentZone'
 
 const contactTemplate = (props) => {
     return (
-        <h1>TEST CONTACT</h1>
+        <div className="one-column-template">
+            <ContentZone name="MainContentZone" {...props} />
+        </div>
     );
 }
 export default contactTemplate;
